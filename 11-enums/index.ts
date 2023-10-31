d4ee0da5321298ed09447b59d2e6221a36ffcbac
Kaue Andrade <kapadilha@minha.fag.edu.br>
2023-10-31 14:47:44 -0300

    add enums folder

---
+++ 11-enums/index.ts
@@ -0,0 +1,15 @@
+enum Planets { // Uma forma de enumerar valores
+  Mercurio,
+  Venus,
+  Terra,
+  Marte,
+}
+
+Planets.Terra
+
+enum Roles {
+  Admin = 'admin',
+  User = 'user',
+}
+
+Roles.Admin
